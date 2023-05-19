d2dc588546259f4dafb43ba90b2d67669c95d883
Enoch Fabiyi <ayomikun204@gmail.com>
2023-05-19 16:35:44 +0100

    restaurant store

--- store.js
+++ store.js
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
-import basketReducer from "./features/basketSlice";
+import basketReducer from "./slices/basketSlice";
+import restaurantReducer from "./slices/restaurantSlice";
 
 export const store = configureStore({
-  reducer: { basket: basketReducer },
+  reducer: { basket: basketReducer, restaurant: restaurantReducer },
 });
