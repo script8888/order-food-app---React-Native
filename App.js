b84ba11bd40ad48b91f88d9ef890da0d2d8438f4
Enoch Fabiyi <ayomikun204@gmail.com>
2023-05-19 16:35:51 +0100

    added basket screen

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import "react-native-url-polyfill/auto";
 import RestaurantScreen from "./screens/RestaurantScreen";
 import { store } from "./store";
 import { Provider } from "react-redux";
+import BasketScreen from "./screens/BasketScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -15,6 +16,11 @@ export default function App() {
         <Stack.Navigator>
           <Stack.Screen name="Home" component={HomeScreen} />
           <Stack.Screen name="Restaurant" component={RestaurantScreen} />
+          <Stack.Screen
+            name="Basket"
+            component={BasketScreen}
+            options={{ presentation: "modal", headerShown: false }}
+          />
         </Stack.Navigator>
       </Provider>
     </NavigationContainer>
