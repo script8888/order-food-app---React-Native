03897c867c20d4c63c7ad1ebc0e3e7e1a84f57fa
Enoch Fabiyi <ayomikun204@gmail.com>
2023-05-19 16:35:56 +0100

    added bsket icon

--- components/Restaurants/BasketIcon.js
+++ components/Restaurants/BasketIcon.js
@@ -1,22 +1,21 @@
 import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
 import { useSelector } from "react-redux";
-import {
-  selectBasketItems,
-  selectBasketTotal,
-} from "../../features/basketSlice";
+import { selectBasketItems, selectBasketTotal } from "../../slices/basketSlice";
 import { useNavigation } from "@react-navigation/native";
 import Currency from "react-currency-formatter";
-import { greenColor } from "../../constants";
 
 export default function BasketIcon() {
   const items = useSelector(selectBasketItems);
   const navigation = useNavigation();
   const basketTotal = useSelector(selectBasketTotal);
 
+  if (items.length === 0) return null;
+  
   return (
     <View className="absolute bottom-10 w-full z-50">
       <TouchableOpacity
+        onPress={() => navigation.navigate("Basket")}
         className={`mx-5 bg-[#00ccbb] p-4 rounded-lg flex-row items-center space-x-1`}
       >
         <Text className="text-white font-extrabold text-lg bg-[#01a296] py-1 px-2">
